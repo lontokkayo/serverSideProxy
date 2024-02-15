6422c4d292441095b058408a675b26f912f3341b
Carl William Chua <carlwilliamchua@gmail.com>
2024-02-15 13:53:06 +0900

    Follow up

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const cheerio = require('cheerio');
-const fs = require('fs');
-const https = require('https');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
