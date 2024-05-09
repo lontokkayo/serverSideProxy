9c1dea284dfb9ce3e23309d3e8fe3f53bde74d2c
Carl William Chua <carlwilliamchua@gmail.com>
2024-05-09 16:36:47 +0900

    05.09.2024

--- rmjFtpServer.js
+++ rmjFtpServer.js
@@ -31,7 +31,7 @@ const PASSWORD = 'U2FsdGVkX18WCFA/fjC/fB6DMhtOOIL/xeVF2tD2b7c=';
 
 const ftpServer = new FtpSrv({
     url: `ftp://${ftpHostname}:${ftpPort}`, // Ensure including the port here
-    pasv_url: '34.97.28.40', // This needs to be reachable from client machines
+    pasv_url: '34.97.229.48', // This needs to be reachable from client machines
     pasv_min: passivePorts.min,
     pasv_max: passivePorts.max,
     anonymous: false,
